146383f132cc3c9041a080c0feb7ffe8d73e0abd
Muhammad <kholilurrohman2001@gmail.com>
2023-02-28 10:15:59 +0700

    type data

---
+++ typeOfData.js
@@ -0,0 +1,11 @@
+let x = 'ini string';
+let y;
+let number = 10;
+let boolean = false;
+
+
+
+console.log(typeof(x))
+console.log(typeof(y))
+console.log(typeof(number))
+console.log(typeof(boolean))
